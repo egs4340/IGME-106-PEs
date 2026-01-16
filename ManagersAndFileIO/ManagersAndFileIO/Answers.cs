964fb349ca5918cb379aa4bef87112219e8b6e48
egs4340 <egs4340@rit.edu>
2026-01-16 11:36:17 -0500

    ManagersAndFileIO

---
+++ ManagersAndFileIO/ManagersAndFileIO/Answers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagersAndFileIO
+{
+    internal class Answers
+    {
+        // This class will not contain any code.
+        // It's entire purpose if a place for students to answer questions posed in the PE description.
+
+        // Answer any questions that are listed in the PE with a question mark:
+
+        // Why is Monster abstract?
+        // Briefly summarize:  What code does the Monster class have/is handling?  
+        // Briefly summarize:  What code do the two child classes have/are handling?  
+        // Why are those classes handling the code that they contain?  (In other words, why was the project architecture designed this way?)
+        // Why is the Random object a field of the Monster class and not a field of the child classes?
+    }
+}
