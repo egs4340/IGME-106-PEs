a54dd1a9bb9bb4222dc367debc52985019769446
egs4340 <egs4340@rit.edu>
2026-01-17 17:08:19 -0500

    ManagersAndFileIO

--- ManagersAndFileIO/ManagersAndFileIO/MonsterManager.cs
+++ ManagersAndFileIO/ManagersAndFileIO/MonsterManager.cs
@@ -6,17 +6,35 @@ using System.Threading.Tasks;
 
 namespace ManagersAndFileIO
 {
+    //Added Class for the Monster Manager
     internal class MonsterManager
     {
+
+        //Added the Stream Readers of the Dragon and Beholder data
+        StreamReader ReadDragonData = null!;
+        StreamReader ReadBeholderData = null!;
         public MonsterManager()
         {
             List<Monster> monsterList = new List<Monster>();
             monsterList.Add(Dragon);
             monsterList.Add(Beholder);
 
+            //Added Try catch for the Stream Readers of the Dragon and Beholder Data
+            try
+            {
+                ReadDragonData = new StreamReader("dragonData.txt");
+
+                ReadBeholderData = new StreamReader("beholderData.txt");
+
+            }
+
+            catch (Exception ex)
+            {
+
+
+            }
 
         }
-
 
     }
 }
