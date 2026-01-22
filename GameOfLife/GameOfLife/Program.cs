b79eb456e85bbc0425ba3903e2b3607306ea9be6
egs4340 <egs4340@rit.edu>
2026-01-22 12:46:57 -0500

    GameOfLife

---
+++ GameOfLife/GameOfLife/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    //Added the Main method
+    static void Main(string[] args)
+    {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
