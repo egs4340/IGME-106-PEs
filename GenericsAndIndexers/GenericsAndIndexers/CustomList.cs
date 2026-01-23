dcd473c5ae048e9bfe8f684cfd54e526ab6297e0
egs4340 <egs4340@rit.edu>
2026-01-23 10:16:19 -0500

    GenericsAndIndexers

---
+++ GenericsAndIndexers/GenericsAndIndexers/CustomList.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAndIndexers
+{
+        /// <summary>
+        /// A list of doubles with custom written methods.
+        /// </summary>
+        internal class CustomList
+        {
+            // --------------------------------------------------------------------
+            // Fields of the class
+            // --------------------------------------------------------------------
+
+            private double[] data;      // Underlying array that holds all list data
+            private int count;          // Size of the list
+
+            // **************************************************
+            // * Answer the following question:                 *
+            // * - Why DON'T we need an extra field to hold     *
+            // *   the list's capacity?                         *
+            // **************************************************
+            // ANSWER(s): 
+            //
+            //
+
+
+            // --------------------------------------------------------------------
+            // Properties of the class
+            // --------------------------------------------------------------------
+
+            /// <summary>
+            /// Returns the current amount of data in the list.
+            /// </summary>
+            public int Count
+            {
+                get { return count; }
+            }
+
+            // **************************************************
+            // * Answer the following questions:                *
+            // * - Why is the Count property get-only?          *
+            // * - Why not include a set?                       *
+            // **************************************************
+            // ANSWER(s): 
+            //
+            //
+
+
+            /// <summary>
+            /// Returns the overall number of elements the internal data structure
+            /// can hold before a resize operation.
+            /// </summary>
+            public int Capacity
+            {
+                get { return data.Length; }
+            }
+
+
+            /*
+            // NOOOOO! Don't do this! This is included to show what NOT to do.
+            public double[] Data
+            {
+                get { return data; }
+                set { data = value; }
+            }
+            */
+            // **************************************************
+            // * Answer the following question:                 *
+            // * - Why DON'T we want a property that gets or    *
+            // *   sets the data array?                         *
+            // **************************************************
+            // ANSWER(s): 
+            //
+            //
+
+
+            // --------------------------------------------------------------------
+            // Indexer Property
+            // --------------------------------------------------------------------
+
+            // TODO: Write an indexer property with get and set
+
+
+
+
+            // --------------------------------------------------------------------
+            // Class Constructors
+            // --------------------------------------------------------------------
+
+            /// <summary>
+            /// Instantiates a new list of doubles with a starting size of 4.
+            /// </summary>
+            public CustomList()
+            {
+                data = new double[4];
+            }
+
+
+            /// <summary>
+            /// Instantiates a new list of doubles with a specified starting size.
+            /// </summary>
+            /// <param name="listSize">Initial size of the list.</param>
+            public CustomList(int listSize)
+            {
+                data = new double[listSize];
+            }
+
+
+            // --------------------------------------------------------------------
+            // Methods
+            // --------------------------------------------------------------------
+
+            /// <summary>
+            /// Add new data to the list.
+            /// </summary>
+            /// <param name="item">Item to add to the next available spot.</param>
+            public void Add(double item)
+            {
+                // **************************************************
+                // * Comment this conditional.                      *
+                // * WHAT does it do and, more importantly,         *
+                // * WHY is it happening?                           *
+                // **************************************************
+                if (count == data.Length - 1)
+                {
+                    IncreaseSizeAndCopyData();
+                }
+
+                // **************************************************
+                // * Comment this code.                             *
+                // * WHAT does it do and, more importantly,         *
+                // * WHY is it happening?                           *
+                // **************************************************
+                data[count] = item;
+                count++;
+            }
+
+
+            // **************************************************
+            // * Answer the following questions:                *
+            // * - What is the purpose of a private method?     *
+            // * - Why is the IncreaseSizeAndCopyData method    *
+            // *   private?                                     *
+            // **************************************************
+            // ANSWER(s): 
+            //
+            //
+
+            /// <summary>
+            /// Doubles the size of the data array.
+            /// </summary>
+            private void IncreaseSizeAndCopyData()
+            {
+                // **************************************************
+                // * Comment this conditional.                      *
+                // * WHAT does it do and, more importantly,         *
+                // * WHY is it happening?                           *
+                // **************************************************
+                if (count != data.Length - 1)
+                {
+                    return;
+                }
+
+                // **************************************************
+                // * Comment every line of this code.               *
+                // * WHAT does it do and, more importantly,         *
+                // * WHY is it happening?                           *
+                // **************************************************
+                double[] largerCopy = new double[data.Length * 2];
+
+                for (int i = 0; i < data.Length; i++)
+                {
+                    largerCopy[i] = data[i];
+                }
+
+                data = largerCopy;
+            }
+
+
+            // **************************************************
+            // * Answer the following questions:                *
+            // * - Where is GetData's thrown exception being    *
+            // *   caught?                                      *
+            // * - Why is the thrown exception NOT caught       *
+            // *   directly in the GetData method?              *
+            // **************************************************
+            // ANSWER(s): 
+            //
+            //
+
+            /// <summary>
+            /// Retrieve data at an index.
+            /// </summary>
+            /// <param name="index">Integer index between 0 and the list's count.</param>
+            /// <returns>Data at a specified index.</returns>
+            /// <exception cref="Exception">Thrown exception when the index is out of range.</excepti
+            public double GetData(int index)
+            {
+                // **************************************************
+                // * Comment this conditional.                      *
+                // * WHAT does it do and, more importantly,         *
+                // * WHY is it happening?                           *
+                // **************************************************
+                if (index >= 0 && index < count)
+                {
+                    return data[index];
+                }
+
+                // **************************************************
+                // * Comment every statement in this code.          *
+                // * WHAT does it do and, more importantly,         *
+                // * WHY is it happening?                           *
+                // **************************************************
+                string exceptionMessage;
+                if (count == 0)
+                {
+                    exceptionMessage = "No data to retrieve, list is empty.";
+                }
+                else
+                {
+                    exceptionMessage = String.Format(
+                        "Index {0} is out of range. Index must be between 0 and {1}",
+                        index,
+                        count - 1);
+                }
+                throw new IndexOutOfRangeException(exceptionMessage);
+            }
+
+
+            // TODO:  Add XML comment for this method
+            public void SetData(int index, double newValue)
+            {
+                // TODO:  Write the body of this method
+            }
+        }
+    }
+
---
+++ GenericsAndIndexers/GenericsAndIndexers/Program.cs
@@ -0,0 +1,172 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GenericsAndIndexers;
+
+Console.WriteLine("Hello, World!");
+static void Main(string[] args)
+{
+    // ------------------------------
+    // ----- Instantiation Test -----
+    // ------------------------------
+
+    // Instantiate a custom list of doubles with capacity of 2.
+    // A capacity of 2 forces a resize quickly.
+    CustomList myList = new CustomList(2);
+
+
+    // ---------------------------------------------------------
+    // ----- GetData/SetData Exceptions Test on Empty List -----
+    // ---------------------------------------------------------
+
+    // TODO: AFTER the list is generic, change all calls to GetData to the indexer.
+    // TODO: AFTER the list is generic, change all calls to SetData to the indexer.
+
+    // Test GetData exception is properly working for an empty list.
+    Console.WriteLine("--> List is empty. Exceptions should occur here... <--");
+    try
+    {
+        Console.WriteLine(myList.GetData(0));
+    }
+    catch (Exception error)
+    {
+        Console.WriteLine("Error occurred: " + error.Message);
+    }
+    // Test SetData exception is properly working for an empty list.
+    try
+    {
+        myList.SetData(0, 12345.6789);
+    }
+    catch (Exception error)
+    {
+        Console.WriteLine("Error occurred: " + error.Message);
+    }
+
+
+    // -----------------------------------
+    // ----- Add values to list Test -----
+    // ------------------------------------
+
+    // Add data to the list
+    myList.Add(2.5);
+    myList.Add(4.2);
+    myList.Add(592);
+    myList.Add(912.68);
+    myList.Add(1943.131);
+
+
+    // ---------------------------
+    // ----- GetData Testing -----
+    // ---------------------------
+
+    // TODO: AFTER the list is generic, change all calls to GetData to the indexer.
+
+    // Print all data in the list by retrieving the value at each index
+    Console.WriteLine("\n--> Values in my list BEFORE modification<--");
+    for (int i = 0; i < myList.Count; i++)
+    {
+        Console.WriteLine(myList.GetData(i));
+    }
+
+
+    // ---------------------------
+    // ----- SetData Testing -----
+    // ---------------------------
+
+    // TODO: AFTER the list is generic, change all calls to SetData to the indexer. 
+    // TODO: AFTER the list is generic, change all calls to GetData to the indexer.
+
+    // Test the SetData method on the first and last items in the list
+    //   and reprint the list's contents for confirmation.
+    myList.SetData(0, 0);
+    myList.SetData(myList.Count - 1, myList.Count - 1);
+
+    Console.WriteLine("\n--> Values in my list AFTER modification <--");
+    for (int i = 0; i < myList.Count; i++)
+    {
+        Console.WriteLine(myList.GetData(i));
+    }
+
+
+    // ----------------------------------------------------
+    // ----- GetData Exceptions with Data in the List -----
+    // ----------------------------------------------------
+
+    // TODO: AFTER the list is generic, change all calls to GetData to the indexer.
+
+    // Test that GetData's exceptions are properly working for
+    //    a list that contains data. Test -1 and a too-large index.
+    Console.WriteLine("\n--> List contains data. " +
+        "Exceptions should occur with invalid indices... <--");
+    try
+    {
+        Console.WriteLine(myList.GetData(-1));
+    }
+    catch (Exception error)
+    {
+        Console.WriteLine("Error occurred: " + error.Message);
+    }
+
+    try
+    {
+        Console.WriteLine(myList.GetData(myList.Count));
+    }
+    catch (Exception error)
+    {
+        Console.WriteLine("Error occurred: " + error.Message);
+    }
+
+
+    // ----------------------------------------------------
+    // ----- GetData Exceptions with Data in the List -----
+    // ----------------------------------------------------
+
+    // TODO: AFTER the list is generic, change all calls to SetData to the indexer.
+
+    // Test that the SetData's exceptions are properly working for
+    //    a list that contains data. Test -1 and a too-large index.
+    try
+    {
+        myList.SetData(-1, 12345.6789);
+    }
+    catch (Exception error)
+    {
+        Console.WriteLine("Error occurred: " + error.Message);
+    }
+
+    try
+    {
+        myList.SetData(myList.Count, 12345.6789);
+    }
+    catch (Exception error)
+    {
+        Console.WriteLine("Error occurred: " + error.Message);
+    }
+
+
+    // **************************************************
+    // * Answer the following questions:                *
+    // *   Using try/catch blocks allows a programmer   *
+    // *   to test a class's exception handling.        *
+    // *   However, it's optimal to PREVENT any crashes *
+    // *   with conditionals in your code when you can. *
+    // * - How could GetData or SetData crashes be      *
+    // *   prevented with conditionals?                 *
+    // * - Explain or write the code.                   *
+    // **************************************************
+    // ANSWER(s): 
+    //
+    //
+
+
+    // ------------------------------------
+    // ----- Count and Capacity Tests -----
+    // ------------------------------------
+
+    // Test the count and capacity properties.
+    Console.WriteLine("\nCount: " + myList.Count);
+    Console.WriteLine("Capacity: " + myList.Capacity);
+
+
+
+
+
+}
