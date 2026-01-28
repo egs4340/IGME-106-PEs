5186f60343ff778513cf7b06d9367f096eaf0af1
egs4340 <egs4340@rit.edu>
2026-01-28 11:51:01 -0500

    LinkedLists

---
+++ LinkedLists/LinkedLists/CustomLinkedList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedLists
+{
+    internal class CustomLinkedList<T>
+    {
+        //Integer for the count of the list
+        private int count;
+
+        //Private CustomLinkedNode set as the head
+        private CustomLinkedNode<T> head;
+
+        //creation of new Linked list
+        LinkedList<T> list = new LinkedList<T>()
+        {
+
+        }
+
+    }
+}
---
+++ LinkedLists/LinkedLists/CustomLinkedNode.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedLists
+{
+    internal class CustomLinkedNode<T>
+    {
+
+        //privte generic data
+        private T data;
+
+        //Next node in the list
+        private CustomLinkedNode<T> next
+        {
+            //Basic get/set for the next class
+            get { return next; }
+
+            set { return; }
+        }
+
+
+        
+
+        //Constructor for the Custom Linked Node
+       public CustomLinkedNode(int data)
+       
+       {
+            //
+            data = 0;
+
+            //
+            next = null;
+       }
+
+
+        
+    }
+}
