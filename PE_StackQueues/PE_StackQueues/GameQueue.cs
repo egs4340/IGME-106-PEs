4bcea8d73f8fe258c5dd73ae686723d3920911e1
egs4340 <egs4340@rit.edu>
2026-02-06 02:13:15 -0500

    Queue and Stack

--- PE_StackQueues/PE_StackQueues/GameQueue.cs
+++ PE_StackQueues/PE_StackQueues/GameQueue.cs
@@ -6,7 +6,80 @@ using System.Threading.Tasks;
 
 namespace PE_StackQueues
 {
-    internal class GameQueue
+    internal class GameQueue<T> : IQueue<T>
     {
+        //added list
+        List<T> list;
+        public GameQueue()
+        {
+            list = new List<T>();
+
+        }
+
+
+        public int Count
+        {
+            get
+            { return list.Count; }
+        }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                //if statement for checking if the code is empty
+                if (Count == 0)
+                { return true; }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        //returns what's at the top of the Queue
+        public T Peek()
+        {
+
+            try
+            {
+                return list[list.Count - 1];
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Cannot peek an empty Queue!");
+                //returns a default value so the pop can run
+                return default(T);
+            }
+        }
+
+        //code for the push method
+        public void Enqueue(T item)
+        {
+            list.Add(item);
+        }
+
+        //code for the pop method
+        public T Dequeue()
+        {
+            try
+            {
+                T item = list[list.Count - 1];
+
+                list.RemoveAt(list.Count - 1);
+
+                return item;
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Can't Dequeue an empty list!");
+                //returns a default value so the pop can run
+                return default(T);
+            }
+
+        }
+
     }
 }
