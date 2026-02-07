da1ff2f86b59359432532924cc61c985c7f93bc5
egs4340 <egs4340@rit.edu>
2026-02-07 16:04:46 -0500

    Dictionary

---
+++ PE_Dictionaries/PE_Dictionaries/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_Dictionaries
+{
+    internal class Player
+    {
+        //added the private ints and strings
+        private int score;
+        private string name;
+
+        //Parameterized constructor
+        public Player(string name, int score)
+        {
+            this.score = score;
+            this.name = name;
+        }
+
+
+    }
+}
---
+++ PE_Dictionaries/PE_Dictionaries/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PE_Dictionaries;
+using System.Threading.Tasks.Sources;
+
+Console.WriteLine("Hello, World!");
+static void Main(string[] args)
+{
+    //Dictionary for the players
+    Dictionary<string, Player> playersDictionary = new Dictionary<string, Player>();
+
+    //creating the new players
+    Player mike = new Player("Mike", 100);
+    Player todd = new Player("Todd", 200);
+
+}
