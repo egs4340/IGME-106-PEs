dbd100fde0a2415d473113932de4b44a53b64c49
egs4340 <egs4340@rit.edu>
2026-02-20 11:40:40 -0500

    SafkoExam1

---
+++ Safko_Practical1/Safko_Practical1/Browser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Safko_Practical1
+{
+    internal class Browser
+    {
+        //data structure for the forwards activity. I made this a list that will eventually get the current webpage and the new webpage, the one we want to proceed towards, and move to it.
+        List<string> dataForwards = new List<string>();
+        
+        //data structure for the backwards activity. I made these lists that will eventually get the current webpage and add the next webpage (previous webpage) and move to it.
+        List<string> dataBackwards = new List<string>();
+
+
+        public Browser() 
+        {
+            
+
+
+        }
+
+        //added the method for visiting a new page
+        public void VisitNewPage(string browser)
+        {
+
+            dataForwards.Add(browser);
+
+
+            dataBackwards.Add(browser);
+        }
+
+
+        //method for moving forwards
+        public void MoveForward()
+        {
+
+        }
+
+        //method for moving backwards
+        public void MoveBackward()
+        {
+
+        }
+
+
+
+    }
+}
---
+++ Safko_Practical1/Safko_Practical1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Safko_Practical1;
+
+    // Local variables needed for successful program run
+    Browser myBrowser = new Browser();
+    string userOption = "";
+
+    // Print the user's menu
+    Console.WriteLine("This menu will appear once at the top, or whenever a user requests to REPEAT it.");
+Console.WriteLine("VISIT a webpage.               Print the CURRENT page.");
+Console.WriteLine("Go BACK to a previous page.    Move FORWARD to the next page.");
+Console.WriteLine("Print the full page HISTORY.   CLEAR the browser history.");
+Console.WriteLine("TOTAL actions by user.         EXIT the program.");
+Console.WriteLine();
+
+// User input loop
+while (userOption != "EXIT")
+{
+    // Retrieve user's choice
+    Console.Write("Choose one of the options: ");
+    userOption = Console.ReadLine()!.ToUpper().Trim();
+
+    switch (userOption)
+    {
+        // Get user's webpage
+        case "VISIT":
+            Console.Write("Enter webpage to visit: ");
+            string page = Console.ReadLine()!;
+    myBrowser.VisitNewPage(page);
+            break;
+
+        // Move forward a page
+        case "FORWARD":
+            myBrowser.MoveForward();
+            break;
+
+        // Move back a page
+        case "BACK":
+            myBrowser.MoveBackward();
+            break;
+
+        // Print all history
+        case "HISTORY":
+            myBrowser.PrintFullHistory();
+            break;
+
+        // Print current browsing page
+        case "CURRENT":
+            myBrowser.PrintCurrentPage();
+            break;
+
+        // Clear browser history
+        case "CLEAR":
+            myBrowser.ClearHistory();
+            break;
+
+        // Amount of user actions
+        case "TOTAL":
+            Console.WriteLine($"User has made {myBrowser.UserActions} actions.");
+            break;
+
+        // "Close" the browser
+        case "EXIT":
+            Console.WriteLine("Goodbye!");
+            break;
+
+        case "REPEAT":
+            // Print the user's menu
+            Console.WriteLine("VISIT a webpage.               Print the CURRENT page.");
+            Console.WriteLine("Go BACK to a previous page.    Move FORWARD to the next page.");
+            Console.WriteLine("Print the full page HISTORY.   CLOSE the browser.");
+            Console.WriteLine("EXIT the program.              REPEAT the menu.");
+            break;
+
+        // Invalid input
+        default:
+            Console.WriteLine("Unrecognized input. Try again.");
+            break;
+        
+
+
+    // Line break for next iteration
+    Console.WriteLine();
+        }
